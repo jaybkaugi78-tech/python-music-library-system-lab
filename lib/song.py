bb6e1790c7bc08d364b9151050a7165ca184ee41
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-06-07 17:28:08 +0300

    finished

--- lib/song.py
+++ lib/song.py
@@ -10,30 +10,37 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-        # Trigger all class methods on creation
-        self.add_song_to_count()
-        self.add_to_genres()
-        self.add_to_artists()
-        self.add_to_genre_count()
-        self.add_to_artists_count()
+        # All class methods trigger on creation
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
 
     @classmethod
     def add_song_to_count(cls):
         cls.count += 1
 
     @classmethod
-    def add_to_genres(cls, genre=None):
-        # Called from __init__ via self, so we need the instance genre
-        pass
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
     @classmethod
-    def add_to_artists(cls, artist=None):
-        pass
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
     @classmethod
-    def add_to_genre_count(cls, genre=None):
-        pass
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
 
     @classmethod
-    def add_to_artists_count(cls, artist=None):
-        pass
+    def add_to_artists_count(cls, artist):
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
