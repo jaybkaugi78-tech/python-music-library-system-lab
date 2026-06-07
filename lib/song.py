f5a085b00589a3cfb8250598359648fdaf7d1567
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-06-07 17:13:27 +0300

    finished

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,39 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        # Trigger all class methods on creation
+        self.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artists_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre=None):
+        # Called from __init__ via self, so we need the instance genre
+        pass
+
+    @classmethod
+    def add_to_artists(cls, artist=None):
+        pass
+
+    @classmethod
+    def add_to_genre_count(cls, genre=None):
+        pass
+
+    @classmethod
+    def add_to_artists_count(cls, artist=None):
+        pass
